cb87d453eac8f6beac8941d8b9b9d0aedbee5fce
mohammad_al_ozeir <mohammad_alozeir@hotmail.com>
2022-06-16 16:20:40 +0300

    minute 36:40

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,22 @@
-const getGoals = (req, res) => {
+const asyncHandler = require("express-async-handler");
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get goals" });
-};
+});
 
-const setGoal = (req, res) => {
-  res.status(200).json({ message: "set goals" });
-};
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("please add a text");
+  }
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT;
 
 const app = express();
+// All to accept body raw (json) data from postman
+app.use(express.json());
+// All to accept urlencoded data from postman
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+// overriding the errorhandler (displays information in postman for example, and to do so, we create errorMiddleware.js)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
